94b8cd36d4edfd3717456b520610fe3fba56652d
noclip64 <rainraygarcia@gmail.com>
2026-05-05 23:39:08 +0800

    forms updates script.js

--- script.js
+++ script.js
@@ -11,33 +11,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
 const form = document.querySelector('form');
 
-form.addEventListener('submit', async (e) => {
-    e.preventDefault(); 
-    
-    const formData = new FormData(form);
-    
-    try {
-        const response = await fetch(form.action, {
-            method: 'POST',
-            body: formData,
-            headers: {
-                'Accept': 'application/json'
-            }
-        });
-
-        if (response.ok) {
-
-            form.reset(); 
-            alert('Message sent! Thank you for the feedback.');
-        } else {
-            const data = await response.json();
-            if (Object.hasOwn(data, 'errors')) {
-                alert(data["errors"].map(error => error["message"]).join(", "));
-            } else {
-                alert('Oops! There was a problem sending your message.');
-            }
-        }
-    } catch (error) {
-        alert('Could not connect to the server. Please check your internet.');
-    }
-});
+form.onsubmit = function() {
+    alert("Message sent! Thank you for the feedback.");
+};
